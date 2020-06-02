f80e36c047e0b6bf70cea15d3a1adafa8c6335dd
Fernanda Ramírez <fernanda.ramirez.olea@gmail.com>
2020-06-02 17:54:32 -0400

    complete y delete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
-import React, {
-  Component
-} from 'react';
-import './App.css';
-import Todos from './components/Todos';
+import React, { Component } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
 
 class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        title: 'Take out the trash',
-        completed: false
+        title: "Take out the trash",
+        completed: false,
       },
       {
         id: 2,
-        title: 'Dinner with wife',
-        completed: false
+        title: "Dinner with wife",
+        completed: false,
       },
       {
         id: 3,
-        title: 'Meeting with boss',
-        completed: false
-      }
-    ]
+        title: "Meeting with boss",
+        completed: false,
+      },
+    ],
   };
-  render() {
-    
-  return( 
-    <div className = "App" >
-      <h1> App </h1>
-      <Todos todos={this.state.todos}/>
-    </div>
-  );
-  }
 
+  markComplete = (id) => {
+    this.setState({
+      todo: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1> App </h1>
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
